cb971e83d27469f47923a526ac74c6db0b9f133b
Kloud333 <leito2005@i.ua>
2017-09-13 12:09:48 +0300

    Add Calculator Commands

---
+++ src/Commands/SubCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace src\Commands;
+
+class SubCommand implements CommandInterface {
+    public function execute(...$args) {
+        if (2 != sizeof($args)) {
+            throw new \InvalidArgumentException('Not enough parameters');
+        }
+
+        return $args[0] - $args[1];
+    }
+}
